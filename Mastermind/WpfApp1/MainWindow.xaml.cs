7273a319912eac48993d28a2391ab042f8cbc29e
jellevannoppenpxl <12401921@student.pxl.be>
2024-11-16 12:00:55 +0100

    MasterMind-03 ComboBoxen

--- Mastermind/WpfApp1/MainWindow.xaml.cs
+++ Mastermind/WpfApp1/MainWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System.ComponentModel.Design.Serialization;
-using System.Drawing;
-using System.Reflection.Emit;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WpfApp1
 {
@@ -19,46 +7,38 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Define available colors and the randomly generated code
+        private readonly List<string> _availableColors = new List<string> { "Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw" };
+        private List<string> _randomCode;
 
         public MainWindow()
         {
-
             InitializeComponent();
-        }
-        private void InitializeGame(object?[] generatedCode)
-        { }
-           
-        
-            private List<string> _availableColors = new List<string> { "Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw" };
-            private List<string> _randomCode;
-
-            public MainWindow()
-            {
-                InitializeComponent();
-                GenerateRandomCode();
-                FillComboBoxes();
-            }
-
-            private void GenerateRandomCode()
-            {
-                Random rnd = new Random();
-                _randomCode = Enumerable.Range(0, 4)
-                    .Select(_ => _availableColors[rnd.Next(_availableColors.Count)])
-                    .ToList();
-
-                this.Title = $"Mastermind - Code: {string.Join(", ", _randomCode)}";
-            }
-
-            private void FillComboBoxes()
-            {
-                var comboBoxes = new[] { comboBox1, comboBox2, comboBox3, comboBox4 };
-                foreach (var comboBox in comboBoxes)
-                {
-                    comboBox.ItemsSource = _availableColors;
-                }
-            }
+            GenerateRandomCode();
+            FillComboBoxes();
         }
 
+        // Generates a random code of 4 colors
+        private void GenerateRandomCode()
+        {
+            Random rnd = new Random();
+            _randomCode = Enumerable.Range(0, 4)
+                .Select(_ => _availableColors[rnd.Next(_availableColors.Count)])
+                .ToList();
+
+            // Display the generated code in the title (for debugging purposes)
+            this.Title = $"Mastermind - Code: {string.Join(", ", _randomCode)}";
+        }
+
+        // Fills ComboBoxes with the available colors
+        private void FillComboBoxes()
+        {
+            // Ensure comboBox1, comboBox2, etc., are defined in XAML
+            var comboBoxes = new[] { comboBox1, comboBox2, comboBox3, comboBox4 };
+            foreach (var comboBox in comboBoxes)
+            {
+                comboBox.ItemsSource = _availableColors;
+            }
+        }
     }
-
-
+}
